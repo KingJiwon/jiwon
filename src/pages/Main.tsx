faaef99fbb0f6532d4090262033153e36e3b285e
KingJiwon <wldnjs0401@naver.com>
2024-08-06 18:38:39 +0900

    08.06

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,5 +2,19 @@ import React from 'react';
 import styles from '../style/pages/main.module.scss';
 
 export default function Main() {
-  return <div className={styles.container}>메인</div>;
+  return (
+    <>
+      <video
+        src="/video/bg-video.mp4"
+        playsInline
+        webkit-playsinline="true"
+        preload="auto"
+        mutedvideo-background
+        className=""
+      >
+        <track kind="captions" />
+      </video>
+      <div className={styles.container}>메인</div>;
+    </>
+  );
 }
