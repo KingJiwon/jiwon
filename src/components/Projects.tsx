421e90baec30467c82f6a030f9173aec9d6f3db0
KingJiwon <wldnjs0401@naver.com>
2024-08-13 17:41:57 +0900

    card flip

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,27 +5,43 @@ import style from '../style/components/Project.module.scss';
 
 gsap.registerPlugin(ScrollTrigger);
 
+interface Project {
+  name: string;
+  skill: string[];
+}
+
 export default function Projects() {
   const containerRef = useRef<HTMLDivElement | null>(null);
   const projectRefs = useRef<HTMLDivElement[]>([]);
-
+  const projects: Project[] = [
+    { name: 'Test-World', skill: ['html5', 'sass', 'javascript'] },
+    {
+      name: 'Dev-City',
+      skill: ['react', 'redux', 'express', 'mongodb', 'sass'],
+    },
+    { name: 'Highlightor', skill: ['next', 'mongodb', 'sass'] },
+    { name: 'Portfolio', skill: ['react', 'typescript', 'sass'] },
+  ];
   useEffect(() => {
     const containerEl = containerRef.current;
     if (!containerEl) return;
 
     projectRefs.current.forEach((project, i) => {
+      // const card = project.children[0];
       gsap.fromTo(
         project,
         {
           y: 100 + i * 50,
           opacity: 0,
           scale: 0.6,
+          rotateY: -180,
         },
         {
           y: -50,
           opacity: 1,
           scale: 1,
-          duration: 1.5,
+          rotateY: 0,
+          duration: 0.8,
           ease: 'power3.out',
           scrollTrigger: {
             trigger: containerEl,
@@ -42,24 +58,26 @@ export default function Projects() {
   return (
     <div className={style.container} ref={containerRef}>
       <div className={style.inner}>
-        {['Test-World', 'Dev-City', 'Highlightor', 'Portfolio'].map(
-          (project, index) => (
-            <div
-              key={project}
-              className={style.skill}
-              ref={(el) => {
-                if (el) projectRefs.current[index] = el;
-              }}
-            >
-              <div className={style.card}>
-                <div className={style.front}>{project}</div>
-                <div className={style.back}>
-                  <p>More info about {project}</p>
-                </div>
+        {projects.map((project, index) => (
+          <div
+            key={project.name}
+            className={style.skill}
+            ref={(el) => {
+              if (el) projectRefs.current[index] = el;
+            }}
+          >
+            <div className={style.card}>
+              <div className={style.front}>{project.name}</div>
+              <div className={style.back}>
+                {project.skill.map((name) => (
+                  <div key={name} className={style.img_container}>
+                    <img alt={name} src={`/icon/${name}.svg`} />
+                  </div>
+                ))}
               </div>
             </div>
-          ),
-        )}
+          </div>
+        ))}
       </div>
     </div>
   );
